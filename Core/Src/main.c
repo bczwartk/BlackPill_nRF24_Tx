ef80390ed6ab50a5230fa65a39f543b873203fe1
Boguslaw Czwartkowski <Boguslaw.Czwartkowski@parasoft.com>
2022-02-12 00:12:02 +0100

    Hal_Delay() uses __WFI() to save clock energy

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -77,6 +77,23 @@ int __io_putchar(int ch)
   return 1;
 }
 
+void HAL_Delay(uint32_t Delay)
+{
+  uint32_t tickstart = HAL_GetTick();
+  uint32_t wait = Delay;
+
+  /* Add a period to guaranty minimum wait */
+  if (wait < HAL_MAX_DELAY)
+  {
+    wait += (uint32_t)uwTickFreq;
+  }
+
+  while ((HAL_GetTick() - tickstart) < wait)
+  {
+	  __WFI();
+  }
+}
+
 void blink_hello()
 {
 	// a few fast blinks to say hello
